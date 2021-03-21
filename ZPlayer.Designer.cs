434cc890dcd41157eabea0c1f069c6c8f5df2631
Roosvelt Medina (Zero) <rzero_dev@outlook.com>
2021-03-21 12:53:10 -0400

    First commit in local repository

---
+++ ZPlayer.Designer.cs
@@ -0,0 +1,322 @@
+﻿
+namespace ZPlayer_v0._1
+{
+    partial class ZPlayer
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ZPlayer));
+            this.bunifuElipse1 = new Bunifu.Framework.UI.BunifuElipse(this.components);
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnAgregar = new Bunifu.Framework.UI.BunifuImageButton();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.lbxLista = new System.Windows.Forms.ListBox();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.btnAdelante = new Bunifu.Framework.UI.BunifuImageButton();
+            this.btnAtras = new Bunifu.Framework.UI.BunifuImageButton();
+            this.btnReproducir = new Bunifu.Framework.UI.BunifuImageButton();
+            this.btnPausar = new Bunifu.Framework.UI.BunifuImageButton();
+            this.btnParar = new Bunifu.Framework.UI.BunifuImageButton();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.btnCerrar = new Bunifu.Framework.UI.BunifuImageButton();
+            this.btnMinimizar = new Bunifu.Framework.UI.BunifuImageButton();
+            this.axWindowsMediaPlayer1 = new AxWMPLib.AxWindowsMediaPlayer();
+            this.bunifuDragControl1 = new Bunifu.Framework.UI.BunifuDragControl(this.components);
+            this.bunifuCustomLabel1 = new Bunifu.Framework.UI.BunifuCustomLabel();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.btnAgregar)).BeginInit();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.btnAdelante)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnAtras)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnReproducir)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnPausar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnParar)).BeginInit();
+            this.panel4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.btnCerrar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnMinimizar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // bunifuElipse1
+            // 
+            this.bunifuElipse1.ElipseRadius = 5;
+            this.bunifuElipse1.TargetControl = this;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(54)))), ((int)(((byte)(90)))));
+            this.panel1.Controls.Add(this.btnAgregar);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(47, 450);
+            this.panel1.TabIndex = 2;
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.BackColor = System.Drawing.Color.Transparent;
+            this.btnAgregar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnAgregar.Image = ((System.Drawing.Image)(resources.GetObject("btnAgregar.Image")));
+            this.btnAgregar.ImageActive = null;
+            this.btnAgregar.Location = new System.Drawing.Point(0, 0);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(47, 43);
+            this.btnAgregar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.btnAgregar.TabIndex = 2;
+            this.btnAgregar.TabStop = false;
+            this.btnAgregar.Zoom = 10;
+            this.btnAgregar.Click += new System.EventHandler(this.bunifuImageButton1_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.panel2.Controls.Add(this.lbxLista);
+            this.panel2.Location = new System.Drawing.Point(45, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(150, 450);
+            this.panel2.TabIndex = 3;
+            // 
+            // lbxLista
+            // 
+            this.lbxLista.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.lbxLista.FormattingEnabled = true;
+            this.lbxLista.Location = new System.Drawing.Point(0, 1);
+            this.lbxLista.Name = "lbxLista";
+            this.lbxLista.Size = new System.Drawing.Size(150, 446);
+            this.lbxLista.TabIndex = 0;
+            this.lbxLista.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.panel3.Controls.Add(this.btnAdelante);
+            this.panel3.Controls.Add(this.btnAtras);
+            this.panel3.Controls.Add(this.btnReproducir);
+            this.panel3.Controls.Add(this.btnPausar);
+            this.panel3.Controls.Add(this.btnParar);
+            this.panel3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.panel3.Location = new System.Drawing.Point(188, 398);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(612, 51);
+            this.panel3.TabIndex = 4;
+            // 
+            // btnAdelante
+            // 
+            this.btnAdelante.BackColor = System.Drawing.Color.Transparent;
+            this.btnAdelante.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnAdelante.Image = ((System.Drawing.Image)(resources.GetObject("btnAdelante.Image")));
+            this.btnAdelante.ImageActive = null;
+            this.btnAdelante.Location = new System.Drawing.Point(352, 7);
+            this.btnAdelante.Name = "btnAdelante";
+            this.btnAdelante.Size = new System.Drawing.Size(38, 37);
+            this.btnAdelante.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.btnAdelante.TabIndex = 7;
+            this.btnAdelante.TabStop = false;
+            this.btnAdelante.Zoom = 10;
+            this.btnAdelante.Click += new System.EventHandler(this.btnAdelante_Click);
+            // 
+            // btnAtras
+            // 
+            this.btnAtras.BackColor = System.Drawing.Color.Transparent;
+            this.btnAtras.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnAtras.Image = ((System.Drawing.Image)(resources.GetObject("btnAtras.Image")));
+            this.btnAtras.ImageActive = null;
+            this.btnAtras.Location = new System.Drawing.Point(212, 7);
+            this.btnAtras.Name = "btnAtras";
+            this.btnAtras.Size = new System.Drawing.Size(38, 37);
+            this.btnAtras.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.btnAtras.TabIndex = 6;
+            this.btnAtras.TabStop = false;
+            this.btnAtras.Zoom = 10;
+            this.btnAtras.Click += new System.EventHandler(this.btnAtras_Click);
+            // 
+            // btnReproducir
+            // 
+            this.btnReproducir.BackColor = System.Drawing.Color.Transparent;
+            this.btnReproducir.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnReproducir.Image = ((System.Drawing.Image)(resources.GetObject("btnReproducir.Image")));
+            this.btnReproducir.ImageActive = null;
+            this.btnReproducir.Location = new System.Drawing.Point(282, 7);
+            this.btnReproducir.Name = "btnReproducir";
+            this.btnReproducir.Size = new System.Drawing.Size(38, 37);
+            this.btnReproducir.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.btnReproducir.TabIndex = 3;
+            this.btnReproducir.TabStop = false;
+            this.btnReproducir.Zoom = 10;
+            this.btnReproducir.Click += new System.EventHandler(this.btnReproducir_Click);
+            // 
+            // btnPausar
+            // 
+            this.btnPausar.BackColor = System.Drawing.Color.Transparent;
+            this.btnPausar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnPausar.Image = ((System.Drawing.Image)(resources.GetObject("btnPausar.Image")));
+            this.btnPausar.ImageActive = null;
+            this.btnPausar.Location = new System.Drawing.Point(317, 7);
+            this.btnPausar.Name = "btnPausar";
+            this.btnPausar.Size = new System.Drawing.Size(38, 37);
+            this.btnPausar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.btnPausar.TabIndex = 4;
+            this.btnPausar.TabStop = false;
+            this.btnPausar.Zoom = 10;
+            this.btnPausar.Click += new System.EventHandler(this.btnPausar_Click);
+            // 
+            // btnParar
+            // 
+            this.btnParar.BackColor = System.Drawing.Color.Transparent;
+            this.btnParar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnParar.Image = ((System.Drawing.Image)(resources.GetObject("btnParar.Image")));
+            this.btnParar.ImageActive = null;
+            this.btnParar.Location = new System.Drawing.Point(247, 7);
+            this.btnParar.Name = "btnParar";
+            this.btnParar.Size = new System.Drawing.Size(38, 37);
+            this.btnParar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.btnParar.TabIndex = 5;
+            this.btnParar.TabStop = false;
+            this.btnParar.Zoom = 10;
+            this.btnParar.Click += new System.EventHandler(this.btnParar_Click);
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(10)))), ((int)(((byte)(10)))), ((int)(((byte)(10)))));
+            this.panel4.Controls.Add(this.bunifuCustomLabel1);
+            this.panel4.Controls.Add(this.btnCerrar);
+            this.panel4.Controls.Add(this.btnMinimizar);
+            this.panel4.Location = new System.Drawing.Point(191, 0);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(609, 37);
+            this.panel4.TabIndex = 5;
+            // 
+            // btnCerrar
+            // 
+            this.btnCerrar.BackColor = System.Drawing.Color.DarkRed;
+            this.btnCerrar.Image = ((System.Drawing.Image)(resources.GetObject("btnCerrar.Image")));
+            this.btnCerrar.ImageActive = null;
+            this.btnCerrar.Location = new System.Drawing.Point(569, 0);
+            this.btnCerrar.Name = "btnCerrar";
+            this.btnCerrar.Size = new System.Drawing.Size(37, 37);
+            this.btnCerrar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.btnCerrar.TabIndex = 10;
+            this.btnCerrar.TabStop = false;
+            this.btnCerrar.Zoom = 10;
+            this.btnCerrar.Click += new System.EventHandler(this.btnCerrar_Click);
+            // 
+            // btnMinimizar
+            // 
+            this.btnMinimizar.BackColor = System.Drawing.Color.Transparent;
+            this.btnMinimizar.Image = ((System.Drawing.Image)(resources.GetObject("btnMinimizar.Image")));
+            this.btnMinimizar.ImageActive = null;
+            this.btnMinimizar.Location = new System.Drawing.Point(526, 0);
+            this.btnMinimizar.Name = "btnMinimizar";
+            this.btnMinimizar.Size = new System.Drawing.Size(42, 37);
+            this.btnMinimizar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.btnMinimizar.TabIndex = 9;
+            this.btnMinimizar.TabStop = false;
+            this.btnMinimizar.Zoom = 10;
+            this.btnMinimizar.Click += new System.EventHandler(this.btnMinimizar_Click);
+            // 
+            // axWindowsMediaPlayer1
+            // 
+            this.axWindowsMediaPlayer1.Enabled = true;
+            this.axWindowsMediaPlayer1.Location = new System.Drawing.Point(191, 33);
+            this.axWindowsMediaPlayer1.Name = "axWindowsMediaPlayer1";
+            this.axWindowsMediaPlayer1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer1.OcxState")));
+            this.axWindowsMediaPlayer1.Size = new System.Drawing.Size(609, 396);
+            this.axWindowsMediaPlayer1.TabIndex = 6;
+            // 
+            // bunifuDragControl1
+            // 
+            this.bunifuDragControl1.Fixed = true;
+            this.bunifuDragControl1.Horizontal = true;
+            this.bunifuDragControl1.TargetControl = this.panel4;
+            this.bunifuDragControl1.Vertical = true;
+            // 
+            // bunifuCustomLabel1
+            // 
+            this.bunifuCustomLabel1.AutoSize = true;
+            this.bunifuCustomLabel1.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bunifuCustomLabel1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.bunifuCustomLabel1.Location = new System.Drawing.Point(10, 9);
+            this.bunifuCustomLabel1.Name = "bunifuCustomLabel1";
+            this.bunifuCustomLabel1.Size = new System.Drawing.Size(66, 21);
+            this.bunifuCustomLabel1.TabIndex = 11;
+            this.bunifuCustomLabel1.Text = "ZPlayer";
+            // 
+            // ZPlayer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.axWindowsMediaPlayer1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "ZPlayer";
+            this.Text = "ZPlayer v0.1";
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.btnAgregar)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.btnAdelante)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnAtras)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnReproducir)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnPausar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnParar)).EndInit();
+            this.panel4.ResumeLayout(false);
+            this.panel4.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.btnCerrar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnMinimizar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Bunifu.Framework.UI.BunifuElipse bunifuElipse1;
+        private System.Windows.Forms.ListBox lbxLista;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel4;
+        private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer1;
+        private Bunifu.Framework.UI.BunifuImageButton btnAgregar;
+        private Bunifu.Framework.UI.BunifuImageButton btnReproducir;
+        private Bunifu.Framework.UI.BunifuImageButton btnCerrar;
+        private Bunifu.Framework.UI.BunifuImageButton btnMinimizar;
+        private Bunifu.Framework.UI.BunifuImageButton btnAtras;
+        private Bunifu.Framework.UI.BunifuImageButton btnParar;
+        private Bunifu.Framework.UI.BunifuImageButton btnPausar;
+        private Bunifu.Framework.UI.BunifuImageButton btnAdelante;
+        private Bunifu.Framework.UI.BunifuDragControl bunifuDragControl1;
+        private Bunifu.Framework.UI.BunifuCustomLabel bunifuCustomLabel1;
+    }
+}
+
---
+++ ZPlayer.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ZPlayer_v0._1
+{
+    public partial class ZPlayer : Form
+    {
+
+        int cursor = 0;
+        String[] archivosMP3;
+        String[] rutaArchivosMP3;
+
+        public ZPlayer()
+        {
+            InitializeComponent();
+        }
+
+        private void bunifuImageButton1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                OpenFileDialog abrir = new OpenFileDialog();
+                abrir.Multiselect = true;
+               
+                if (abrir.ShowDialog() == DialogResult.OK)
+                {
+                    archivosMP3 = abrir.SafeFileNames;
+                    rutaArchivosMP3 = abrir.FileNames;
+                    foreach (var mp3 in archivosMP3)
+                    {
+                        lbxLista.Items.Add(mp3);
+
+                    }
+                    axWindowsMediaPlayer1.URL = rutaArchivosMP3[0];
+                    lbxLista.SelectedIndex = 0;
+                }
+            }
+            catch (Exception error)
+            {
+
+                MessageBox.Show($"Se ha producido un error de tipo: {error.Message.ToLower()}");
+
+            }
+        }
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            axWindowsMediaPlayer1.URL = rutaArchivosMP3[lbxLista.SelectedIndex];
+
+            cursor = lbxLista.SelectedIndex; //Actualiza la posición del cursor.
+        }
+
+        private void btnCerrar_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnMinimizar_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void btnReproducir_Click(object sender, EventArgs e)
+        {
+            axWindowsMediaPlayer1.Ctlcontrols.play();
+        }
+
+        private void btnPausar_Click(object sender, EventArgs e)
+        {
+            axWindowsMediaPlayer1.Ctlcontrols.pause();
+        }
+
+        private void btnParar_Click(object sender, EventArgs e)
+        {
+            axWindowsMediaPlayer1.Ctlcontrols.stop();
+        }
+
+        private void btnAtras_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                cursor--;
+
+                if (cursor < 0)
+                {
+                    cursor = rutaArchivosMP3.Length -1;
+                    axWindowsMediaPlayer1.URL = rutaArchivosMP3[cursor];
+                }
+                else
+                {
+                    axWindowsMediaPlayer1.URL = rutaArchivosMP3[cursor];
+                }
+            }
+            catch (Exception error)
+            {
+
+                MessageBox.Show($"Se ha producido un error de tipo: {error.Message.ToLower()}");
+            }
+        }
+
+        private void btnAdelante_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                cursor++;
+
+                if (cursor > rutaArchivosMP3.Length -1)
+                {
+                    cursor = 0;
+                    axWindowsMediaPlayer1.URL = rutaArchivosMP3[cursor];
+                    lbxLista.SelectedItem = cursor;
+                }
+                else
+                {
+                    axWindowsMediaPlayer1.URL = rutaArchivosMP3[cursor];
+                }
+            }
+            catch (Exception error)
+            {
+
+                MessageBox.Show($"Se ha producido un error de tipo: {error.Message.ToLower()}");
+            }
+        }
+    }
+}
